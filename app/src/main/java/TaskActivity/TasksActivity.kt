472cc7c7de694b32bbcc89d62d4b5a9639d1a929
srbayard <79609996+srbayard@users.noreply.github.com>
2021-03-17 14:32:57 -0500

    Created task activity. Moved NeighborhoodTalk Activity to main app folder.

---
+++ app/src/main/java/TaskActivity/TasksActivity.kt
@@ -0,0 +1,11 @@
+package TaskActivity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TasksActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tasks)
+    }
+}
