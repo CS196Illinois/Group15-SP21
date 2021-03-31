9981983d00cc042ec380a7aad1c164f59688498f
Gomez, Alex <aegomez3@illinois.edu>
2021-03-31 13:26:48 -0500

    continued work on firebase, commiting my side of stuff including button that uploads basic info

--- app/src/main/java/com/example/cs196/MainActivity.kt
+++ app/src/main/java/com/example/cs196/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.example.cs196
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.cs196.databinding.ActivityMainBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    val db = Firebase.firestore
+    private lateinit var mAuth: FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            var user = hashMapOf(
+                "password" to "castaway",
+                "user" to "cat"
+            )
+            val users = db.collection("users")
+            users.document("user").set(user)
+        }
+
+        // auth stuff
+        mAuth = FirebaseAuth.getInstance()
+        val googleUser = mAuth.currentUser
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .build()
+
+        var mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+
+        if (account != null) {
+            //return already signed in TODO
+        }
+        fun signIn() {
+            val signInIntent = mGoogleSignInClient.getSignInIntent()
+        }
+        binding.button2.setOnClickListener {
+
+        }
     }
+
+
+
 }
