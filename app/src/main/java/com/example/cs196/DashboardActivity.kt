c1fcdf746e4aa34f97ce4d5c0f4da910846ee081
AymanTheTinkerer <aymanismail2001@gmail.com>
2021-04-17 11:00:31 -0500

    Updated test suite

--- app/src/main/java/com/example/cs196/DashboardActivity.kt
+++ app/src/main/java/com/example/cs196/DashboardActivity.kt
@@ -1,15 +1,26 @@
 package com.example.cs196
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.activity_dashboard.*
+import java.util.*
 
 class DashboardActivity : AppCompatActivity() {
 
     lateinit var EnterCourseName: EditText
     lateinit var AddCourse: Button
+    lateinit var context : Context
+    lateinit var alarmManager : AlarmManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +31,31 @@ class DashboardActivity : AppCompatActivity() {
 
         AddCourse.setOnClickListener {
             saveCourse()
+        }
+
+        context = this
+        alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        create.setOnClickListener {
+            val second = set_timer.text.toString().toInt() * 1000
+            val intent = Intent(context,Receiver::class.java)
+            val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + second, pendingIntent)
+            Log.d("DashboardActivity", "Create : " + Date().toString())
+        }
+
+        update.setOnClickListener {
+            val second = set_timer.text.toString().toInt() * 1000
+            val intent = Intent(context,Receiver::class.java)
+            val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + second, pendingIntent)
+            Log.d("DashboardActivity", "Update : " + Date().toString())
+        }
+
+        cancel.setOnClickListener {
+            val intent = Intent(context,Receiver::class.java)
+            val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+            Log.d("DashboardActivity", "Cancel : " + Date().toString())
+            alarmManager.cancel(pendingIntent)
         }
     }
 
@@ -36,6 +72,11 @@ class DashboardActivity : AppCompatActivity() {
         val course = Course(courseID, name)
 
         ref.child(courseID).setValue(course)
+    }
 
+    class Receiver : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            Log.d("DashboardActivity", "Received message")
+        }
     }
 }
---
+++ app/src/main/java/com/example/cs196/MyFirebaseMessagingService.kt
@@ -0,0 +1,10 @@
+package com.example.cs196
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    override fun onNewToken(token: String) {
+        Log.d("TAG", "Refreshed token: $token")
+    }
+}
